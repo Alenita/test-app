e415636854cc6a45184efe3dc3f6f2fc4ea36078
Alena Gorshkova <alenagorsh@gmail.com>
2020-02-09 12:40:37 +0300

    Updates

---
+++ precache-manifest.330d6f9d623353cb0e7136d9b301a6bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6d95568d30926dae31b092075ba1be",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "247af2b7b8c3ed4ce43d",
+    "url": "/test-app/static/css/main.718c8706.chunk.css"
+  },
+  {
+    "revision": "3dc7a0c64248d18e860d",
+    "url": "/test-app/static/js/2.203d1424.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/test-app/static/js/2.203d1424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "247af2b7b8c3ed4ce43d",
+    "url": "/test-app/static/js/main.76cf52d6.chunk.js"
+  },
+  {
+    "revision": "a7685168259b13e7085a",
+    "url": "/test-app/static/js/runtime-main.350dc61c.js"
+  }
+]);
